33ee7dcb4da8bc7a79ee9f7e2d1ea9a377c8d7f0
diep.huynh <it.huynhdiep@gmail.com>
2023-10-05 21:33:09 +0700

    .

--- test.py
+++ test.py
@@ -1,28 +1,46 @@
+import time
 import os
 import pandas as pd 
 import unittest
 import openpyxl
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 import re
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import constants
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from openpyxl import Workbook
+import csv
+from selenium.common.exceptions import NoSuchElementException
 
 class PythonOrgSearch(unittest.TestCase):
 
     def setUp(self):
-        chrome_options = Options()
-        # chrome_options.add_argument("--headless")
-        self.driver = webdriver.Chrome(options=chrome_options)
+        firefox_options = Options()
+        # firefox_options.add_argument("--headless")
+        self.driver = webdriver.Firefox(options=firefox_options)
+        self.driver.maximize_window()
+
+        # Define folder paths
+        result_folder_path = os.path.join("results", constants.YEAR_MONTH_FOLDER)
+        errors_folder_path = os.path.join("results", constants.YEAR_MONTH_FOLDER, "errors")  
+        screenshot_folder_path = "errors/screenshots"  # Assuming you want it in the current working directory
+
+        # Combine the current working directory with the screenshot folder
+        screenshot_folder_path = os.path.join(os.getcwd(), screenshot_folder_path)
+
+        # Create directories if they do not exist
+        for folder_path in [result_folder_path, errors_folder_path, screenshot_folder_path]:
+            if not os.path.exists(folder_path):
+                os.makedirs(folder_path)
 
 ###################### common ###############################
     def extract_text(self, element):
         return element.text
     
     def convert_to_string(self, string): 
-        return "`" + string;
+        return "`" + string
     
     def trim_text(self, element):
         element = element.lstrip()
@@ -201,6 +219,8 @@ class PythonOrgSearch(unittest.TestCase):
     
 ##################### End common ################################
     
+    
+    
 
 ####################### 16a1 ##############################
     def process_model_16a1(self, code):
@@ -213,9 +233,10 @@ class PythonOrgSearch(unittest.TestCase):
         date = self.get_date()
         contents = self.driver.find_elements(By.CSS_SELECTOR, '.jrcel[class*="cel_0_"]')
         money = self.driver.find_elements(By.CSS_SELECTOR, '.jrcel[class*="cel_soTien_"]')
-        workbook_name = "results/16a1.xlsx"
+        workbook_name = "results/" + constants.YEAR_MONTH_FOLDER + "/16a1.xlsx"
 
         return self.save_information_workbook_16a1_16c1(code, number, unit, receiver, sender_acc, receiver_acc, location, date, contents, money, workbook_name)
+    
 ####################### 16a1 ##############################
 
     
@@ -232,7 +253,7 @@ class PythonOrgSearch(unittest.TestCase):
         paid = self.driver.find_elements(By.CSS_SELECTOR, '.jrcel[class*="cel_7_"]')
         location = self.get_content_by_key_search("Tại KBNN (NH):")
         date = self.get_date()
-        workbook_name = "results/16a2.xlsx"
+        workbook_name = "results/" + constants.YEAR_MONTH_FOLDER +  "/16a2.xlsx"
 
         return self.save_information_workbook_16a2_16c(code, number, unit, sender_acc, receiver_acc, contents, total, tax, paid, receiver, location, date, workbook_name)
 ####################### End 16a2 ##############################
@@ -250,7 +271,7 @@ class PythonOrgSearch(unittest.TestCase):
         paid = self.driver.find_elements(By.CSS_SELECTOR, '.jrcel[class*="cel_5_"]')
         location = self.get_content_by_key_search("Tại Kho bạc Nhà nước (NH):")
         date = self.get_date()
-        workbook_name = "results/16c.xlsx"
+        workbook_name = "results/" + constants.YEAR_MONTH_FOLDER +  "/16c.xlsx"
 
         return self.save_information_workbook_16a2_16c(code, number, unit, sender_acc, receiver_acc, contents, total, tax, paid, receiver, location, date, workbook_name)
 ####################### End 16c ##############################
@@ -266,9 +287,9 @@ class PythonOrgSearch(unittest.TestCase):
         money = self.driver.find_elements(By.CSS_SELECTOR, '.jrcel[class*="cel_3_"]')
         location = self.get_content_by_key_search("Tại Kho bạc Nhà nước (NH):")
         date = self.get_date()
-        workbook_name = "results/16c1.xlsx"
+        workbook_name = "results/" + constants.YEAR_MONTH_FOLDER +  "/16c1.xlsx"
 
-        return self.save_information_workbook_16a1_16c1(code, number, unit, receiver, sender_acc, receiver_acc, location, date, contents, money, workbook_name)
+        return  self.save_information_workbook_16a1_16c1(code, number, unit, receiver, sender_acc, receiver_acc, location, date, contents, money, workbook_name)
 ####################### End 16c1 ##############################
 
 ######################### 07 #################################
@@ -301,79 +322,159 @@ class PythonOrgSearch(unittest.TestCase):
             money.append(money_text)
             contents.append(elements[i+5].text)
             i += 12
-        workbook_name = "results/07.xlsx"
+        workbook_name = "results/" + constants.YEAR_MONTH_FOLDER +   "/07.xlsx"
 
         return self.save_information_workbook_07(code, number, unit, code_unit, bill_code, bill_date, dos_code, dos_date, NDKT_code, contents, money, date, workbook_name)       
 ######################### 07 #################################
 
 ######################### Main #################################
+    def login(self):
+        self.driver.get(constants.LOGIN_URL)
+        # time.sleep(5)
+        self.wait_for_element(By.XPATH, '//input[@id="r1:0:pt1:sf1:it1::content"]')
+        self.enter_text(By.XPATH, '//input[@id="r1:0:pt1:sf1:it1::content"]', constants.USER_NAME)
+        self.enter_text(By.XPATH, '//input[@id="r1:0:pt1:sf1:it2::content"]', constants.PASSWORD)
+        self.click_element(By.XPATH, '//button[@id="r1:0:pt1:sf1:cbLogin"]')
 
+        self.driver.get(constants.URL_AFTER_LOGIN)
+
+        self.click_element(By.XPATH, '//button[@id="pt1:r1:0:cb3"]')
+        self.wait_for_element(By.XPATH, '//input[@id="pt1:r1:0:it12::content"]')
+        self.enter_text(By.XPATH, '//input[@id="pt1:r1:0:it12::content"]', constants.YEAR)
+        self.enter_text(By.XPATH, '//input[@id="pt1:r1:0:it10::content"]', constants.YEAR_MONTH)
+
+        time.sleep(20) #TODO = 100 seconds
     
+        self.click_element(By.XPATH, '//button[@id="pt1:r1:0:cb4"]')
+        # time.sleep(10) #TODO = 100 seconds 
+
+    def wait_for_element(self, by, locator):
+        WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located((by, locator)))
+
+    def enter_text(self, by, locator, text):
+        element = self.driver.find_element(by, locator)
+        element.send_keys(text)
+
+    def click_element(self, by, locator):
+        element = self.driver.find_element(by, locator)
+        element.click()
+        time.sleep(10)
+
 
     def get_list_href(self):
-        self.driver.get("file:///D:/01Projects/03KhoBac/khobac-crawler/types/main.html")
-        table_element = self.driver.find_element(By.CSS_SELECTOR, 'table.x11e')
-        links = table_element.find_elements(By.CSS_SELECTOR, ".xh7 a")
-
+        links = self.driver.find_elements(By.XPATH, '//a[@class="xfh"]')
         hrefs = []
         i = 1
-        while i < len(links) and i + 6 < len(links): 
+        while i < len(links) and i + 6 <= len(links): 
             hrefs.append(links[i])
             i = i + 6
-
         return hrefs
-    
+
+    def handle_error(self): 
+        self.driver.refresh()
+        time.sleep(10) #TODO = 1000
+
+
     def handle_click_event(self, items):
-        i = 1
-        for item in items:
-            item.click()
-            print(item.text)
-            i = i + 1
-        print(i)
+        i = 0
+        j = 0
+        while i < len(items):
+            try:
+                items = self.get_list_href()# Remove blocking and modal glass panes if they exist
 
-    def detect_iframe(self):
-        code_value = self.get_content_by_key_search("Mẫu số")
+                for class_name in ['AFBlockingGlassPane', 'AFModalGlassPane']:
+                    try:
+                        element = self.driver.find_element(By.XPATH, f'//div[@class="{class_name}"]')
+                        self.driver.execute_script("arguments[0].parentNode.removeChild(arguments[0]);", element)
+                    except NoSuchElementException:
+                        pass
 
-        if code_value == constants.MAU_SO_16a1:
-            return self.process_model_16a1(code_value)
+                item_id = items[i].get_attribute("id")
 
+                # Find the element you want to wait for
+                WebDriverWait(self.driver, 10).until(
+                    EC.element_to_be_clickable((By.ID, item_id))
+                )
+
+                items[i].click()
+                time.sleep(5)
+                self.handle_iframe(i, j)
+                time.sleep(5)
+                i += 1  # Increment i for the next iteration
+
+                if (i)  == len(items):
+                    next_button = self.driver.find_element(By.XPATH, '//a[@id="pt1:r1:0:cl1"]')
+
+                    if 'p_AFDisabled' in next_button.get_attribute('class'):
+                        break  # Exit the loop if the next button is disabled
+                    else:
+                        i = 0  # Reset i to 0 for the next iteration
+                        j = j + 1
+                        print("Move to next page", j)
+                        next_button.click()
+                        time.sleep(5)
+
+            except Exception as e:
+                print(f"Loop: {e}")
+                self.handle_error()
+
+    def handle_close_modal(self):
+        close = self.driver.find_element(By.XPATH, '//div[@id="ctb1"]//a')
+        return close.click()
+
+    def handle_iframe(self, i, j):
+        self.driver.switch_to.frame(1)
+        try: 
+            code_value = self.get_content_by_key_search("Mẫu số")
+        except Exception as e:
+            print(f"Loop: {e}")
+
+        if code_value in [constants.MAU_SO_04a, constants.MAU_SO_04b, constants.MAU_SO_05]:
+            print("04 or 05")
+            time.sleep(5)
+            self.handle_close_modal()
+            return self.driver.switch_to.parent_frame()
+        
+        elif code_value == constants.MAU_SO_16a1:
+            self.process_model_16a1(code_value)
+        
         elif code_value == constants.MAU_SO_16a2:
-            return self.process_model_16a2(code_value)
+            self.process_model_16a2(code_value)
 
         elif code_value == constants.MAU_SO_16c:
-            return self.process_model_16c(code_value)
+            self.process_model_16c(code_value)
         
         elif code_value == constants.MAU_SO_16c1:
-            return self.process_model_16c1(code_value)
+            self.process_model_16c1(code_value)
 
         elif code_value == constants.MAU_SO_07:
-            return self.process_model_07(code_value)
+            self.process_model_07(code_value)
 
         else:
-            return True
+            print("Invalid case")
+            index = i + 1 
+            result_array = [(index, j, code_value)]
+            headers_to_check = ["ID", "Page", "Code"]
+            self.export_file("errors/output.xlsx", headers_to_check, result_array)
 
+        time.sleep(5)
+        # self.take_screen_shot(i)
+        self.handle_close_modal()
+        return self.driver.switch_to.parent_frame()
+
+    # def take_screen_shot(self, i):
+    #     self.driver.save_screenshot('errors/screenshots/' + str(i) + ".png")
         
     def test_process_page(self):
+        self.login()
+        hrefs = self.get_list_href()
+        self.handle_click_event(hrefs)
 
-        self.driver.get("file:///D:/01Projects/03KhoBac/khobac-crawler/types/kbst.html")
-        self.driver.get("file:///D:/01Projects/03KhoBac/khobac-crawler/types/16a1.html")
-        # self.driver.get("file:///D:/01Projects/03KhoBac/khobac-crawler/types/16a2.html")
-        # self.driver.get("file:///D:/01Projects/03KhoBac/khobac-crawler/types/16c.html")
-        # self.driver.get("file:///D:/01Projects/03KhoBac/khobac-crawler/types/16c1.html")
-        # self.driver.get("file:///D:/01Projects/03KhoBac/khobac-crawler/types/07.html")
-        # self.driver.get("file:///D:/01Projects/03KhoBac/khobac-crawler/types/0701.html")
-        # self.driver.get("file:///D:/01Projects/03KhoBac/khobac-crawler/types/main.html")
-
-
-        # hrefs = self.get_list_href()
-        # self.handle_click_event(hrefs)
-
-        
 ######################### End #################################
 
 
     # def tearDown(self):
-    #     self.driver.quit()
+        # self.driver.quit()
 
 
 if __name__ == "__main__":
