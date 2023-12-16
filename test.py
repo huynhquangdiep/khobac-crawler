2adf205d888924393d333fd7a3fc483c9a4dead6
diep.huynh <it.huynhdiep@gmail.com>
2023-12-16 12:12:27 +0700

    a

--- test.py
+++ test.py
@@ -17,24 +17,17 @@ from selenium.common.exceptions import NoSuchElementException
 class PythonOrgSearch(unittest.TestCase):
 
     def setUp(self):
-        # Create a FirefoxOptions instance
-        options = webdriver.FirefoxOptions()
-        # Set the log_output preference to specify desired logs
-        options.set_preference("log_output", "logs.txt")
-        
-        # Initialize the WebDriver with the options
-        self.driver = webdriver.Firefox(options=options)
+        firefox_options = Options()
+        # firefox_options.add_argument("--headless")
+        self.driver = webdriver.Firefox(options=firefox_options)
         self.driver.maximize_window()
 
         # Define folder paths
         result_folder_path = os.path.join("results", constants.YEAR_MONTH_FOLDER)
-        errors_folder_path = os.path.join("results", constants.YEAR_MONTH_FOLDER, "errors")  
-        screenshot_folder_path = "errors/screenshots"  # Assuming you want it in the current working directory
-        # Combine the current working directory with the screenshot folder
-        screenshot_folder_path = os.path.join(os.getcwd(), screenshot_folder_path)
+        errors_folder_path = os.path.join(constants.YEAR_MONTH_FOLDER, "errors")  
 
         # Create directories if they do not exist
-        for folder_path in [result_folder_path, errors_folder_path, screenshot_folder_path]:
+        for folder_path in [result_folder_path, errors_folder_path]:
             if not os.path.exists(folder_path):
                 os.makedirs(folder_path)
 
@@ -166,150 +159,9 @@ class PythonOrgSearch(unittest.TestCase):
         ]  
             
         return self.export_file(workbook_name, headers_to_check, result_array)
-    ##################### End common ################################
+##################### End common ################################
     
     
-    ######################### Main #################################
-    def login(self):
-        self.driver.get(constants.LOGIN_URL)
-        # time.sleep(5)
-        self.wait_for_element(By.XPATH, '//input[@id="r1:0:pt1:sf1:it1::content"]')
-        self.enter_text(By.XPATH, '//input[@id="r1:0:pt1:sf1:it1::content"]', constants.USER_NAME)
-        self.enter_text(By.XPATH, '//input[@id="r1:0:pt1:sf1:it2::content"]', constants.PASSWORD)
-        self.click_element(By.XPATH, '//button[@id="r1:0:pt1:sf1:cbLogin"]')
-
-        self.driver.get(constants.URL_AFTER_LOGIN)
-
-        self.click_element(By.XPATH, '//button[@id="pt1:r1:0:cb3"]')
-        self.wait_for_element(By.XPATH, '//input[@id="pt1:r1:0:it12::content"]')
-        self.enter_text(By.XPATH, '//input[@id="pt1:r1:0:it12::content"]', constants.YEAR)
-        self.enter_text(By.XPATH, '//input[@id="pt1:r1:0:it10::content"]', constants.YEAR_MONTH)
-
-        time.sleep(20) #TODO = 100 seconds
-    
-        self.click_element(By.XPATH, '//button[@id="pt1:r1:0:cb4"]')
-        # time.sleep(10) #TODO = 100 seconds 
-
-    def wait_for_element(self, by, locator):
-        WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located((by, locator)))
-
-    def enter_text(self, by, locator, text):
-        element = self.driver.find_element(by, locator)
-        element.send_keys(text)
-
-    def click_element(self, by, locator):
-        element = self.driver.find_element(by, locator)
-        element.click()
-        time.sleep(10)
-
-
-    def get_list_href(self):
-        links = self.driver.find_elements(By.XPATH, '//a[@class="xfh"]')
-        hrefs = []
-        i = 1
-        while i < len(links) and i + 6 <= len(links): 
-            hrefs.append(links[i])
-            i = i + 6
-        return hrefs
-
-    def handle_error(self): 
-        print("Reload page")
-        self.driver.refresh()
-        time.sleep(10)
-
-    def remove_unnecessary_element(self):
-        for class_name in ['AFBlockingGlassPane', 'AFModalGlassPane']:
-            try:
-                element = self.driver.find_element(By.XPATH, f'//div[@class="{class_name}"]')
-                self.driver.execute_script("arguments[0].parentNode.removeChild(arguments[0]);", element)
-            except NoSuchElementException:
-                pass
-
-    def handle_click_event(self, items):
-        i = 0
-        j = 0
-        while i < len(items):
-            try:
-                items = self.get_list_href()
-                
-                # Remove blocking and modal glass panes if they exist
-                self.remove_unnecessary_element()
-                
-                item_id = items[i].get_attribute("id")
-
-                # Find the element you want to wait for
-                WebDriverWait(self.driver, 10).until(
-                    EC.element_to_be_clickable((By.ID, item_id))
-                )
-
-                items[i].click()
-                time.sleep(5)
-                self.handle_iframe(i, j)
-                time.sleep(5)
-                i += 1  # Increment i for the next iteration
-
-                if (i)  == len(items):
-                    next_button = self.driver.find_element(By.XPATH, '//a[@id="pt1:r1:0:cl1"]')
-
-                    if 'p_AFDisabled' in next_button.get_attribute('class'):
-                        break  # Exit the loop if the next button is disabled
-                    else:
-                        i = 0  # Reset i to 0 for the next iteration
-                        j = j + 1
-                        print("Move to next page", j)
-                        next_button.click()
-                        time.sleep(5)
-
-            except Exception as e:
-                print(f"Loop: {e}")
-                self.handle_error()
-
-    def handle_close_modal(self):
-        close = self.driver.find_element(By.XPATH, '//div[@id="ctb1"]//a')
-        close.click()
-        time.sleep(2)
-
-    def switch_to_iframe(self, index):
-        try:
-            self.driver.switch_to.frame(index)
-        except Exception as e:
-            print(f"Failed to switch to iframe: {e}")
-            self.handle_error()
-
-    def handle_invalid_case(self, i, j, code_value):
-        print("Invalid case")
-        index = i + 1 
-        result_array = [(index, j, code_value)]
-        headers_to_check = ["ID", "Page", "Code"]
-        self.export_file("errors/output.xlsx", headers_to_check, result_array)
-
-    def handle_iframe(self, i, j):
-        try: 
-            self.switch_to_iframe(1)
-            code_value = self.get_content_by_key_search("Mẫu số")
-        except Exception as e:
-            print(f"Failed to handle iframe: {e}")
-            self.handle_error()
-
-        handlers = {
-            constants.MAU_SO_04a: self.handle_close_modal,
-            constants.MAU_SO_04b: self.handle_close_modal,
-            constants.MAU_SO_05: self.handle_close_modal,
-            constants.MAU_SO_16a1: self.process_model_16a1,
-            constants.MAU_SO_16a2: self.process_model_16a2,
-            constants.MAU_SO_16c: self.process_model_16c,
-            constants.MAU_SO_16c1: self.process_model_16c1,
-            constants.MAU_SO_07: self.process_model_07
-        }
-
-        handler = handlers.get(code_value, self.handle_invalid_case)
-        handler(code_value)
-        self.driver.switch_to.parent_frame()
-
-        time.sleep(5)
-        self.handle_close_modal()
-        return self.driver.switch_to.parent_frame()
-        
 ####################### 16a1 ##############################
     def process_model_16a1(self, code):
         number = self.get_content_by_key_search("Số:")
@@ -326,9 +178,8 @@ class PythonOrgSearch(unittest.TestCase):
         return self.save_information_workbook_16a1_16c1(code, number, unit, receiver, sender_acc, receiver_acc, location, date, contents, money, workbook_name)
     
 ####################### 16a1 ##############################
-
     
-    ####################### Function of 16a2 ######################
+####################### Function of 16a2 ######################
     def process_model_16a2(self, code):
         number = self.get_content_by_key_search("Số:")
         unit = self.get_content_by_key_search("Đơn vị rút dự toán:")
@@ -415,11 +266,149 @@ class PythonOrgSearch(unittest.TestCase):
         return self.save_information_workbook_07(code, number, unit, code_unit, bill_code, bill_date, dos_code, dos_date, NDKT_code, contents, money, date, workbook_name)       
 ######################### 07 #################################
 
+######################### Main #################################
+    def login(self):
+        self.driver.get(constants.LOGIN_URL)
+        # time.sleep(5)
+        self.wait_for_element(By.XPATH, '//input[@id="r1:0:pt1:sf1:it1::content"]')
+        self.enter_text(By.XPATH, '//input[@id="r1:0:pt1:sf1:it1::content"]', constants.USER_NAME)
+        self.enter_text(By.XPATH, '//input[@id="r1:0:pt1:sf1:it2::content"]', constants.PASSWORD)
+        self.click_element(By.XPATH, '//button[@id="r1:0:pt1:sf1:cbLogin"]')
+        self.driver.get(constants.URL_AFTER_LOGIN)
+        self.click_element(By.XPATH, '//button[@id="pt1:r1:0:cb3"]')
+        self.wait_for_element(By.XPATH, '//input[@id="pt1:r1:0:it12::content"]')
+        self.enter_text(By.XPATH, '//input[@id="pt1:r1:0:it12::content"]', constants.YEAR)
+        self.enter_text(By.XPATH, '//input[@id="pt1:r1:0:it10::content"]', constants.YEAR_MONTH)
 
+        time.sleep(60) #TODO = 100 seconds
+    
+        self.click_element(By.XPATH, '//button[@id="pt1:r1:0:cb4"]')
+        # time.sleep(10) #TODO = 100 seconds 
+
+    def wait_for_element(self, by, locator):
+        WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located((by, locator)))
+
+    def enter_text(self, by, locator, text):
+        element = self.driver.find_element(by, locator)
+        element.send_keys(text)
+
+    def click_element(self, by, locator):
+        element = self.driver.find_element(by, locator)
+        element.click()
+        time.sleep(10)
+
+
+    def get_list_href(self):
+        links = self.driver.find_elements(By.XPATH, '//a[@class="xfh"]')
+        hrefs = []
+        i = 1
+        while i < len(links) and i + 6 <= len(links): 
+            hrefs.append(links[i])
+            i = i + 6
+        return hrefs
+
+    def handle_error(self): 
+        print("Reload page")
+        self.driver.refresh()
+        time.sleep(4)
+
+    def remove_unnecessary_element(self):
+        for class_name in ['AFBlockingGlassPane', 'AFModalGlassPane']:
+            try:
+                element = self.driver.find_element(By.XPATH, f'//div[@class="{class_name}"]')
+                self.driver.execute_script("arguments[0].parentNode.removeChild(arguments[0]);", element)
+            except NoSuchElementException:
+                pass
+
+    def handle_click_event(self, items):
+        i = 0
+        j = 0
+        while i < len(items):
+            try:
+                items = self.get_list_href()# Remove blocking and modal glass panes if they exist
+
+                # Remove blocking and modal glass panes if they exist
+                self.remove_unnecessary_element()
+
+                item_id = items[i].get_attribute("id")
+
+                # Find the element you want to wait for
+                WebDriverWait(self.driver, 10).until(
+                    EC.element_to_be_clickable((By.ID, item_id))
+                )
+
+                items[i].click()
+                time.sleep(5)
+                self.handle_iframe(i, j)
+                time.sleep(5)
+                i += 1  # Increment i for the next iteration
+
+                if (i)  == len(items):
+                    next_button = self.driver.find_element(By.XPATH, '//a[@id="pt1:r1:0:cl1"]')
+
+                    if 'p_AFDisabled' in next_button.get_attribute('class'):
+                        break  # Exit the loop if the next button is disabled
+                    else:
+                        i = 0  # Reset i to 0 for the next iteration
+                        j = j + 1
+                        print("Move to next page", j)
+                        next_button.click()
+                        time.sleep(5)
+
+            except Exception as e:
+                print(f"Loop: {e}")
+                self.handle_error()
+
+    def handle_close_modal(self):
+        close = self.driver.find_element(By.XPATH, '//div[@id="ctb1"]//a')
+        return close.click()
+
+    def handle_iframe(self, i, j):
+        try: 
+            self.driver.switch_to.frame(1)
+            code_value = self.get_content_by_key_search("Mẫu số")
+        except Exception as e:
+            print(f"Loop: {e}")
+            self.handle_error()
+
+        if code_value in [constants.MAU_SO_04a, constants.MAU_SO_04b, constants.MAU_SO_05]:
+            print("04 or 05")
+            time.sleep(5)
+            self.handle_close_modal()
+            return self.driver.switch_to.parent_frame()
+        
+        elif code_value == constants.MAU_SO_16a1:
+            self.process_model_16a1(code_value)
+        
+        elif code_value == constants.MAU_SO_16a2:
+            self.process_model_16a2(code_value)
+
+        elif code_value == constants.MAU_SO_16c:
+            self.process_model_16c(code_value)
+        
+        elif code_value == constants.MAU_SO_16c1:
+            self.process_model_16c1(code_value)
+
+        elif code_value == constants.MAU_SO_07:
+            self.process_model_07(code_value)
+
+        else: # TODO not save the code yet
+            print("Invalid case")
+            index = i + 1 
+            result_array = [(index, j, code_value)]
+            headers_to_check = ["ID", "Page", "Code"]
+            self.export_file("errors/output.xlsx", headers_to_check, result_array)
+
+        time.sleep(5)
+        self.handle_close_modal()
+        return self.driver.switch_to.parent_frame()
+        
     def test_process_page(self):
-        self.login() # TODO
+        self.login()
         hrefs = self.get_list_href()
         self.handle_click_event(hrefs)
+
+######################### End #################################
 
 
     # def tearDown(self):
